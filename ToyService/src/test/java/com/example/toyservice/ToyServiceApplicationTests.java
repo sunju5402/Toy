0bb045b043e166a2ab2b55cebd86fc2d76eaf72d
sunju5402 <ssonju5402@gamil.com>
2022-12-25 22:43:00 +0900

    project setup

---
+++ ToyService/src/test/java/com/example/toyservice/ToyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
