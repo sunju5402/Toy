d9496cef8d89808d75cee0080dc7070d8270cf43
sunju5402 <ssonju5402@gamil.com>
2023-01-04 00:47:49 +0900

    초기화설정 redix, swagger, 이메일 인증을 위한 gmail 설정

---
+++ ToyService/src/main/java/com/example/toyservice/web/ToyServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.toyservice.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class ToyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyServiceApplication.class, args);
+	}
+
+}
